9c3ad11f6fb497fe1aa0d63f4e73ac8b5d1f3840
Luka <lukianeshavladze@gmail.com>
2023-04-06 02:29:03 +0400

    data analize

--- getting_data.py
+++ getting_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.lines import Line2D
 
 
 url2 = 'https://www.basketball-reference.com/leagues/NBA_2023_per_game.html#per_game_stats'
@@ -12,7 +13,7 @@ for i in headers.columns:
     headers_list.append(i)
 # print(headers_list)
 npdata = np.array(data[0])
-rows = list(range(1, 688))
+rows = list(range(1, 698))
 columns = list(range(1, 31))
 df = pd.DataFrame(npdata, rows, headers_list)
 df = df.drop_duplicates()
@@ -47,15 +48,12 @@ df['PF'] = df['PF'].astype(float)
 df['PTS'] = df['PTS'].astype(float)
 
 
-# pd.set_option('display.max_rows', None)
-
-
-
+# pd.set_option('display.max_columns', None)
 
 boston_top = df[(df['Tm'] == 'BOS') & (df['PTS'] > 11)]
 
 boston = df[(df['Tm'] == 'BOS')]
-atlanda = df[(df['Tm'] == 'ATL')]
+atlanta = df[(df['Tm'] == 'ATL')]
 houston = df[(df['Tm'] == 'HOU')]
 memphis = df[(df['Tm'] == 'MEM')]
 denver = df[(df['Tm'] == 'DEN')]
@@ -85,20 +83,41 @@ orlando = df[(df['Tm'] == 'ORL')]
 detroit = df[(df['Tm'] == 'DET')]
 charlotte = df[(df['Tm'] == 'CHO')]
 
-print(milwaukee)
 
-# sns.scatterplot(x='PTS', y='Player',  hue='Pos', palette='bright', data=boston)
+# print(milwaukee[milwaukee['Pos'] == 'SG'])
+
+def show_top_players(team_name, position, sorted_by, numb_players=2):
+    team_pos = (team_name[team_name['Pos'] == position].sort_values(sorted_by, ascending=False).head(numb_players))
+    print(team_pos)
+
+
+# test = boston[(boston['Pos'] == 'PG') | (boston['Pos'] == 'SG')]
+# print(test)
+def every_position_player_bypts(team):
+    pg = show_top_players(team, 'PG', 'PTS', 2)
+    sg = show_top_players(team, 'SG', 'PTS', 2)
+    c = show_top_players(team, 'C', 'PTS', 2)
+    pf = show_top_players(team, 'PF', 'PTS', 2)
+    sf = show_top_players(team, 'SF', 'PTS', 2)
+    print(pg, sg, c, pf, sf)
+
+every_position_player_bypts(milwaukee)
+
+
+# bos_PG = (boston[boston['Pos'] == 'PG'].sort_values('PTS', ascending=False).head(2))
+# print(bos_PG)
+
+# sns.scatterplot(x='PTS', y='Player',  hue='Pos', palette='bright', data=milwaukee.sort_values('Pos'))
 # plt.show()
-sns.scatterplot(x='PTS', y='Player',  hue='Pos', palette='bright', data=milwaukee)
-plt.show()
+
+# a = Line2D([], [], color='blue', label='PG')
+# b = Line2D([], [], color='green', label='SG')
+# c = Line2D([], [], color='purple', label='PF')
+# d = Line2D([], [], color='red', label='SF')
+# e = Line2D([], [], color='yellow', label='C')
+# plt.legend(handles=[a, b, c, d, e])
+# sns.histplot(x='PTS', y='Player', hue='Pos', palette=(a._color, b._color, c._color, d._color, e._color), data=milwaukee.sort_values('PTS'))
+# plt.show()
 
 # sns.lmplot(data=boston, x='PTS', y='BLK', hue='Pos')
 # plt.show()
-
-
-# print(boston_top)
-
-
-
-
-
